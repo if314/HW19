fdb473f1b66201ce5abb2f973200ca04c2d4f9b1
i <keranovai@gmail.com>
2020-01-14 16:16:20 -0500

    js reaserch charts
    added js chars to pakrme.html

---
+++ vendor/bootstrap/js/charts.js
@@ -0,0 +1,64 @@
+new Chart(document.getElementById("horizontalBar"), {
+"type": "horizontalBar",
+"data": {
+"labels": ["I get frustrated looking for parking", "I don't want to pay for parking ", "I can't find parking", "I never carry cash", "I am late for work"],
+"datasets": [{
+"label": "How consumers feel about parking",
+"data": [64, 48, 38, 12, 20],
+"fill": false,
+"backgroundColor": ["rgba(255, 99, 132, 0.2)", "rgba(255, 159, 64, 0.2)",
+"rgba(255, 205, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(54, 162, 235, 0.2)",
+"rgba(153, 102, 255, 0.2)", "rgba(201, 203, 207, 0.2)"
+],
+"borderColor": ["rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)",
+"rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)", "rgb(201, 203, 207)"
+],
+"borderWidth": 1
+}]
+},
+"options": {
+"scales": {
+"xAxes": [{
+"ticks": {
+"beginAtZero": true
+}
+}]
+}
+}
+});
+
+
+//pie
+var ctxP = document.getElementById("pieChart").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+type: 'pie',
+data: {
+labels: ["Not Important", "Very Important", "Important", "Not Very Important"],
+datasets: [{
+data: [6, 54, 36, 3],
+backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+}]
+},
+options: {
+responsive: true
+}
+});
+
+
+//doughnut
+var ctxD = document.getElementById("doughnutChart").getContext('2d');
+var myLineChart = new Chart(ctxD, {
+type: 'doughnut',
+data: {
+labels: ["Easy", "Somewhat Easy", "Not easy, not hard", "Hard", "Nightmare"],
+datasets: [{
+data: [9, 21, 21, 24, 24],
+backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+}]
+},
+options: {
+responsive: true
+}
+});
